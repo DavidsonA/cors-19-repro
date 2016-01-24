cb1aed326274d89c3d5bc750e670622d4fefebc6
DavidsonA <zliipsgv@grandmasmail.com>
2016-01-25 09:36:12 +1000

    Repro for CORS issue 19

---
+++ src/ExampleApi/Models/Foo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleApi.Models
+{
+    public class Foo
+    {
+        public int Bar { get; set; }
+        public string Baz { get; set; }
+    }
+}
