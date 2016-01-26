2257250b8c3228669adc5cbc0139c174385c7f19
Doug Bunting <dougbu@microsoft.com>
2016-01-26 14:55:33 -0800

    Nits: Remove un-needed files, `using`s and settings - add `.gitignore` to ignore VS and output files going forward

--- src/ExampleApi/Startup.cs
+++ src/ExampleApi/Startup.cs
@@ -1,7 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Cors.Infrastructure;
 using Microsoft.AspNet.Hosting;
